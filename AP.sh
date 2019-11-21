47d164eae157381241eb05b92597de31ce18fb63
yuuki2028 <35856683+yuuki2028@users.noreply.github.com>
2019-11-21 20:27:21 +0900

    Update AP.sh

--- AP.sh
+++ AP.sh
@@ -1,7 +1,7 @@
-git config --global user.name "beet-aizu"
-git config --global user.email "aki.bdash@gmail.com"
+git config --global user.name "yuuki2028"
+git config --global user.email "yuuki1420@outlook.jp"
 
-git remote set-url origin https://beet-aizu:${GITHUB_TOKEN}@github.com/beet-aizu/github_actions.git
+git remote set-url origin https://beet-aizu:37040ba2f6c56d4efcbf603d48cf5a62d8152cd7@github.com/beet-aizu/github_actions.git
 
 git checkout -b master
 git branch -a
