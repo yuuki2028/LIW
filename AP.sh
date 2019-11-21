6dfb1f15f1f52922f9acac9178599610469ebf01
yuuki2028 <35856683+yuuki2028@users.noreply.github.com>
2019-11-21 21:09:56 +0900

    Update AP.sh

--- AP.sh
+++ AP.sh
@@ -5,5 +5,5 @@ git config --global user.email "yuuki1420@outlook.jp"
 git config --global user.name "yuuki2028"
 git add .
 git commit -m "Auto Push"
-git remote add origin https://github.com/yuuki2028/LIW.git
+git remote add origin https://github.com/yuuki2028/LIW
 git push origin master
